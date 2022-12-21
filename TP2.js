3177447ea00533ec4a55614e430cabc038deb499
rosierludivine <ludivine.rosier38@gmail.com>
2022-12-21 10:50:57 +0100

    pause

--- TP2.js
+++ TP2.js
@@ -22,7 +22,7 @@
 //     }
 // // Ecrire dans le fichier (nom du fichier , le changement voulu , type d'ecriture)
 //     fs.writeFile('text.txt', 'Le changement a été fait ', 'utf-8', (err) =>{
-//         if (err) {
+//         if (err) {//si erreur 
 //                 console.log(err)//si erreur 
 //                 return// afficher l'erreur 
 //         }
@@ -53,17 +53,64 @@
 
 
 // Écrivez un programme Node.js qui utilise le module fs pour lister les fichiers d'un répertoire.
-const fs = require('fs');
+// const fs = require('fs');// Immorter le module fs
 
+// fs.readdir("exercice3", (err, files) => {// Creation de la fonction readdir
+//     if (err)//Si erreur 
+//       console.log(err);// afficher err
+//     else {// sinon
+//       console.log("\nList files in directory :");// afficher list files in directory 
+//       files.forEach(file => { //Pour chaque fichier 
+//         console.log(file);// afficher le nom du fichier 
+//       })
+//     }
+//   })
 
 // Écrivez un programme Node.js qui utilise le module fs pour créer un nouveau fichier et y écrire des données.
 
+// const fs = require('fs');// Immorter le module fs
+// fs.writeFile('exercice4.txt', 'Exercice4', (error) =>{ 
+//   if (error){
+//     console.log(error)
+//     return;
+//   }
+//   console.log('fichier créer') 
+
+// })// Pas compris ce programme 
+
 // Écrivez un programme Node.js qui utilise le module os pour obtenir le répertoire personnel de l'utilisateur actuel.
+// const fs = require('fs');//module fs
+// const os = require('os');//modume os 
+
+// console.log(os.homedir());
+// fs.readdir((os.homedir()), (err, files) => {// Creation de la fonction readdir prenant le chemin du repertoire personnel de l'utilisateur actuel grâce au os
+//         if (err)//Si erreur 
+//           console.log(err);// afficher err
+//         else {// sinon
+//           console.log("\nList files in directory :");// afficher list files in directory 
+//           files.forEach(file => { //Pour chaque fichier 
+//             console.log(file);// afficher le nom du fichier 
+//           })
+//         }
+//       })
 
 // Écrivez un programme Node.js qui utilise le module child_process pour exécuter un programme en ligne de commande.
 
-// Écrivez un programme Node.js qui utilise le module path pour manipuler les chemins de fichiers.
+// // Écrivez un programme Node.js qui utilise le module path pour manipuler les chemins de fichiers.
+// const path = require ('path');// module Path
+
+
+// console.log(path.basename('Semaine_19decembre/text.txt', '.txt'));
+// // Returns: 'text', il retourne que le nom du fichier sans lextension 
+
+// console.log(process.env.PATH);
+// // return l'emplacement de l'environnement path
+
+// console.log(path.dirname('/Semaine_19decembre/exercice4.txt'));
+
 
 // Écrivez un programme Node.js qui utilise le module querystring pour analyser une chaîne de requête URL.
+
+
 
 // Écrivez un programme Node.js qui utilise le module events pour créer un émetteur d'événements personnalisé.
